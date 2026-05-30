fea67b0fb9edc045617830b87ca999e96b9125ad
aashish <ritikkamboj4314@gmail.com>
2026-05-30 17:02:37 +0530

    Fix : instead of using direct service link , we assign variable and use it.

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 
 const { BookingRepository } = require("../repositories");
+const {ServerConfig} = require("../config")
 
 const db = require("../models");
 
@@ -8,7 +9,7 @@ async function createBooking(data) {
   try {
     const result = db.sequelize.transaction(async function bookingImpl(t) {
       const flight = await axios.get(
-        `http://localhost:3000/api/v1/flights/${data.flightId}`,
+        `${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`,
       );
       console.log(flight.data);
       return true;
