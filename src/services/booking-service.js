231cfad5a8562f4ed6b2250c4f74c3d52eaf60fc
aashish <ritikkamboj4314@gmail.com>
2026-05-30 19:50:31 +0530

    feat: use of unmanaged transaction approach

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -8,29 +8,32 @@ const AppError = require("../utils/errors/app-error");
 const { StatusCodes } = require("http-status-codes");
 
 async function createBooking(data) {
+  const transaction = await db.sequelize.transaction();
   try {
-    const result =  await db.sequelize.transaction(async function bookingImpl(t) {
-      const flight = await axios.get(
-        `${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`,
-      );
-      const flightData = flight.data.data;
+    const flight = await axios.get(
+      `${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`,
+    );
+    const flightData = flight.data.data;
 
-      if (data.noOfSeats <flightData.totalSeats) {
-        throw new AppError("Required Number of Seats not availaible",StatusCodes.BAD_REQUEST);
-      }
-      // console.log(flight.data);
-      // console.log(
-      //   flight.data.totalSeats,
-      //   data.noOfSeats,
-      //   "in booking service ",
-      //   flight.data.price,
-      // );
-      return true;
-    });
-    console.log(result);
-    return result ;
+    if (data.noOfSeats > flightData.totalSeats) {
+      throw new AppError(
+        "Required Number of Seats not availaible",
+        StatusCodes.BAD_REQUEST,
+      );
+    }
+    // console.log(flight.data);
+    // console.log(
+    //   flight.data.totalSeats,
+    //   data.noOfSeats,
+    //   "in booking service ",
+    //   flight.data.price,
+    // );
+    await transaction.commit();
+    return true;
   } catch (error) {
     console.log("error catched in booking service catch");
+    await transaction.rollback();
+
     throw error;
   }
 }
