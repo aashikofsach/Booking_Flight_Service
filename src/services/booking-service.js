3b4454a53417265835afee61dd9efa97d59dd381
aashish <ritikkamboj4314@gmail.com>
2026-05-31 14:36:21 +0530

    feat : created booking by using transaction in booking and flights service

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -3,9 +3,11 @@ const axios = require("axios");
 const { BookingRepository } = require("../repositories");
 const { ServerConfig } = require("../config");
 
+const bookingrepository = new BookingRepository() ;
 const db = require("../models");
 const AppError = require("../utils/errors/app-error");
 const { StatusCodes } = require("http-status-codes");
+const { FLIGHT_SERVICE } = require("../config/server-config");
 
 async function createBooking(data) {
   const transaction = await db.sequelize.transaction();
@@ -21,6 +23,9 @@ async function createBooking(data) {
         StatusCodes.BAD_REQUEST,
       );
     }
+    const totalBillAmount = data.noOfSeats * flightData.price;
+    console.log(totalBillAmount, "");
+    const bookingPayload = { ...data, totalCost: totalBillAmount };
     // console.log(flight.data);
     // console.log(
     //   flight.data.totalSeats,
@@ -28,8 +33,17 @@ async function createBooking(data) {
     //   "in booking service ",
     //   flight.data.price,
     // );
+
+    const booking = await bookingrepository.createBooking(bookingPayload, transaction);
+    // now here booking done and so as we have to update the avalaible seats in flight 
+    // for that we have to make request to flight service 
+    await axios.patch(`${FLIGHT_SERVICE}/api/v1/flights/${data.flightId}/seats`, {
+      seats : data.noOfSeats
+    })
+
+
     await transaction.commit();
-    return true;
+    return booking;
   } catch (error) {
     console.log("error catched in booking service catch");
     await transaction.rollback();
