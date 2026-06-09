8cc13506590147766bd0d2c20f89a1144eba0d0a
aashish <ritikkamboj4314@gmail.com>
2026-06-09 10:56:39 +0530

    feat: add AMQP connection setup and queue initialization (producer created to broker queue)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const {PORT} = require("./config");
 const express = require('express');
+const amqplib= require("amqplib");
+
+async function queueConnection()
+{
+    try {
+        const connection = await amqplib.connect("amqp://localhost") ;
+        const channel = await connection.createChannel() ;
+
+        await channel.assertQueue("noti-queue");
+        // setInterval(() => {
+        //              channel.sendToQueue("noti-queue", Buffer.from("Something to do "))
+
+            
+        // }, 1000);
+        
+    } catch (error) {
+        console.log('errror during connection to rabbit MQ', error);
+        
+    }
+}
 const apiRoutes = require("./routes")
 const {ServerConfig , logger} = require("./config");
 const CRONS = require("./utils/common/cron-jobs")
@@ -16,6 +36,7 @@ app.use("/api", apiRoutes)
 app.listen(ServerConfig.PORT , () => {
     console.log(`The server is running on the PORT: ${ServerConfig.PORT}`) ;
     CRONS();
+    queueConnection();
    
 })
 
